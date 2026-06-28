27df4da58f2c5007cf139ccb650131b2cdfcd72d
TANARAT <tanarat.dev18@gmil.com>
2026-06-28 22:12:29 +0700

    DB CONFIG

--- src/config/database.ts
+++ src/config/database.ts
@@ -7,7 +7,8 @@ export const pool = new Pool({
     host: process.env.DB_HOST ?? 'localhost',
     user: process.env.DB_USER ?? 'postgres',
     password: process.env.DB_PASSWORD ?? '123456',
-    database: process.env.DB_NAME ?? 'ba_tung'
+    database: process.env.DB_NAME ?? 'ba_tung',
+    port: Number(process.env.DB_PORT ?? '5432'),
 });
 
 export let dbError: string | null = null;
